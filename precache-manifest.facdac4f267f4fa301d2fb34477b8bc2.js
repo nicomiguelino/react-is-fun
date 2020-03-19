e6b33387dc2a24020ec8e427a7b88fb0c18368b6
Nico Miguelino <nicomiguelino2014@gmail.com>
2020-03-19 22:00:46 +0800

    Updates

---
+++ precache-manifest.facdac4f267f4fa301d2fb34477b8bc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3753ce6fe0f585eb032c6b6ad66cd6",
+    "url": "/react-is-fun/index.html"
+  },
+  {
+    "revision": "a4158dbbc005138b8a4b",
+    "url": "/react-is-fun/static/js/2.73d7eb29.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-is-fun/static/js/2.73d7eb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9512823cd2883d69c908",
+    "url": "/react-is-fun/static/js/main.1c572fec.chunk.js"
+  },
+  {
+    "revision": "4bc34c36f0508fec3182",
+    "url": "/react-is-fun/static/js/runtime-main.713ab4e3.js"
+  }
+]);
